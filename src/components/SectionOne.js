f821aed0be82d5a72a21e48b4a541b164f97a8f0
Emmanuel <emmanuel@cbqasolutions.com>
2019-05-06 10:52:09 -0500

    Spring Animation was added

--- src/components/SectionOne.js
+++ src/components/SectionOne.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import { Spring } from "react-spring/renderprops";
 
 export default class SectionOne extends Component {
   render() {
+
+    const textStyle = {
+      color: '#000000'
+    }
+
     return (
       <div className="section">
-        <h3>Section one</h3>
+      <Spring
+      from={{ opacity: 0, marginTop: -10000 }}
+      to={{ opacity: 1, marginTop: 0 }}
+      >
+        {props => (
+          <div style={props}>
+              <h3 style={textStyle}>Section one</h3>
+          </div>
+        )}
+      </Spring>
       </div>
     );
   }
